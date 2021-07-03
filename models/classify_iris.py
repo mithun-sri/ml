4f445cbbacc052df02c36d8896cf42ac5f23703a
Mithun Sri <lnsmirth@gmail.com>
2021-07-03 06:16:43 +0530

    Added kNN algorithm which makes predictions for randomised dataset and returns test accuracy.

--- models/classify_iris.py
+++ models/classify_iris.py
@@ -1,17 +1,31 @@
 import sys
 import pandas as pd
-import matplotlib
 import numpy as np
-import scipy as sp
-import IPython
-import sklearn
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 import mglearn
+from sklearn.neighbors import KNeighborsClassifier
 
 if __name__ == "__main__":
     iris_dataset = datasets.load_iris()
-    X_train, X_test, y_train, y_test = train_test_split(iris_dataset['data'], iris_dataset['target'], random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(iris_dataset['data'], iris_dataset['target'],
+                                                        random_state=np.random)
+
+    # Produces a dataframe and scatter matrix which can be used for visualisation needs
+
     iris_dataframe = pd.DataFrame(X_train, columns=iris_dataset.feature_names)
-    grr = pd.plotting.scatter_matrix(iris_dataframe, c=y_train, figsize=(15, 15), marker='o', hist_kwds={'bins': 20}, s=60,
-                                     alpha=.8, cmap=mglearn.cm3)
+    grr = pd.plotting.scatter_matrix(iris_dataframe, c=y_train, figsize=(15, 15), marker='o', hist_kwds={'bins': 20},
+                                     s=60, alpha=.8, cmap=mglearn.cm3)
+
+    # Using k-Nearest Neighbors to produce a classification algorithm and fitting training dataset
+    knn = KNeighborsClassifier(n_neighbors=1)
+    knn.fit(X_train, y_train)
+
+    # Algorithm makes predictions for test data and outputs predictions
+
+    y_prediction = knn.predict(X_test)
+    print("Test set predictions:\n {}".format(y_prediction))
+
+    # Output test set accuracy in predictions
+
+    print("Test set score: {:.2f}".format(knn.score(X_test, y_test)))
